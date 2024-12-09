d60509d6fe523d090a16389eca55c10ecd2a55e7
Cyrus987 <tfish3029@gmail.com>
2024-12-09 14:48:08 -0800

    Update and rename 6DevinneyCyrus.java to sixDevinneyCyrus.java

--- sixDevinneyCyrus.java
+++ sixDevinneyCyrus.java
@@ -8,9 +8,9 @@ import java.util.*;
  * You can create additional methods but must have the move method
  * 
  */
-public class 6DevinneyCyrus implements Player
+public class sixDevinneyCyrus implements Player
 {
-    private static String name = "6DevinneyCyrus";
+    private static String name = "sixDevinneyCyrus";
     /**
      * An example of a method - replace this comment with your own
      * You must create some kind of logic of what to play against your opponent...start thinking!
